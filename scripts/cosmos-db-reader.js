e67bb434092bf833432aee04d5752de3f8da08e0
Anthony Verevkin <anthony@verevkin.ca>
2024-07-13 00:39:00 -0400

    disable proxy

--- scripts/cosmos-db-reader.js
+++ scripts/cosmos-db-reader.js
@@ -5,12 +5,12 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
 class CosmosDBReader {
     constructor(endpoint, databaseId, containerId) {
-        const http_proxy = process.env.http_proxy;
-        const proxyAgent = new HttpsProxyAgent(http_proxy);
+//        const http_proxy = process.env.http_proxy;
+//        const proxyAgent = new HttpsProxyAgent(http_proxy);
 
         this.client = new CosmosClient({
             endpoint,
-            agent: proxyAgent,
+//            agent: proxyAgent,
             aadCredentials: new DefaultAzureCredential()
         });
         this.databaseId = databaseId;
